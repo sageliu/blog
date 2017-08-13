b9889925cae2f4c75425d869c42e98888768e919
liupingyin <liupingyin@xjgreat.com>
2017-08-13 10:41:41 +0800

    应用模板，注意是相对路径

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ let express = require('express');
 let router=express.Router();
 
 router.get('/signup',function (req,res) {
-
-  res.send('获取注册表单')
+  //渲染模板第一个参数是相对路径，相对于模板的根目录
+  //第二个参数是给模板传的数据，模板中用<%=title%>使用
+  res.render('user/signup',{title:'用户注册'})
 })
 router.post('/signup',function (req,res) {
   res.send()
